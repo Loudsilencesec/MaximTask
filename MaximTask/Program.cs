6f9247e77596efb423ed5cd8f9dd96b37d49719c
Loudsilencesec <loudsilencesec@gmail.com>
2023-04-19 16:53:19 +0500

    Добавьте файлы проекта.

---
+++ MaximTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MaximTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(task1(input));
+
+        }
+
+        static string task1(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                string half1 = str.Substring(0, str.Length /2);
+                string life2 = str.Substring(str.Length /2);
+
+                half1 = new string(half1.Reverse().ToArray());
+                life2 = new string(life2.Reverse().ToArray());
+
+                return half1 + life2;
+            }
+
+            string reversstr = new string(str.Reverse().ToArray());
+
+            return reversstr + str;
+            
+        }
+    }
+}
